6d55bab6b8f758f79e3452558cfa4262b4f9cb6b
Rider21 <58046032+Rider21@users.noreply.github.com>
2024-06-16 18:54:17 +0300

    fix crash

--- android/app/src/main/java/com/rajarsheechatterjee/LNReader/MainApplication.kt
+++ android/app/src/main/java/com/rajarsheechatterjee/LNReader/MainApplication.kt
@@ -33,7 +33,7 @@ class MainApplication : Application(), ReactApplication {
             return packages
           }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+          override fun getJSMainModuleName(): String = "main"
 
           override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
